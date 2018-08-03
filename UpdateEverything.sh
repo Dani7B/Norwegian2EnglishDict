1c872196f8c4cc83ebed7c023d01d4e8d8360bf6
Martin Rys <spleefer90@gmail.com>
2018-08-03 16:04:46 +0200

    make script more friendly

--- UpdateEverything.sh
+++ UpdateEverything.sh
@@ -5,6 +5,8 @@ mkdir -p ~/wiktionaryDump
 if [[ ! -h 2_wiktionaryDump ]]; then
 	ln -s ~/wiktionaryDump 2_wiktionaryDump
 else
+	rm 2_wiktionaryDump # If it exists remove and recreate the symlink. This is to make sure it's pointed to the right home folder in case someone clones this repo.
+	ln -s ~/wiktionaryDump 2_wiktionaryDump
 	rm -f ./2_wiktionaryDump/NB/*.json # While old entries get overwritten by scrap.py, some may have been removed totally from Wiktionary,
 	rm -f ./2_wiktionaryDump/NN/*.json #	so remove all files before downloading
 fi
