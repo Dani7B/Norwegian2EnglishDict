ed8ddb0ad404ed42fdd19ec4210d51fdb95f8c87
C0rn3j <spleefer90@gmail.com>
2018-09-20 23:07:49 +0200

    better workaround for FW bugs

--- 3_definitionsAndInflections/json2txt_inf.php
+++ 3_definitionsAndInflections/json2txt_inf.php
@@ -54,13 +54,14 @@ function scrapShit($languageToScrap) {
 			// For every inflection line member except the first one
 			for ($k = 1; $k < count($inflectionExplode);$k++) {
 				// If current inflection member matches the current word in a word definition line
-				if ($inflectionExplode[$k] == $wordDefExplode[0]){
+				if (removeAccents($inflectionExplode[$k]) == removeAccents($wordDefExplode[0])){
 					// For every word definition line
 					for($l = 0; $l < count($finalWordDefinitionArray); $l++) {
 						$wordDefExplode2 = explode("	", $finalWordDefinitionArray[$l], 2);
+//						if(removeAccents($wordDefExplode2[0]) == removeAccents($inflectionExplode[0])) {
 						if($wordDefExplode2[0] == $inflectionExplode[0]) {
-							echo "Definition for $inflectionExplode[0] will be copied to word $wordDefExplode[0]; Which means it'll look like |$wordDefExplode[0]	$wordDefExplode2[1]| COWABUNGA\n";
-							array_push($tempCatWordDefArray, $wordDefExplode[0]."	".$wordDefExplode2[1]);
+								echo "Definition for $inflectionExplode[0] will be copied to word $inflectionExplode[$k]; Which means it'll look like |$inflectionExplode[$k]	$wordDefExplode2[1]| COWABUNGA\n";
+							array_push($tempCatWordDefArray, $inflectionExplode[$k]."	".$wordDefExplode2[1]);
 						}
 					}
 				}
@@ -100,6 +101,7 @@ function scrapShit($languageToScrap) {
 					// For every word definition line
 					for($l = 0; $l < count($finalWordDefinitionArray); $l++) {
 						$wordDefExplode2 = explode("	", $finalWordDefinitionArray[$l], 2);
+//						if(removeAccents($wordDefExplode2[0]) == removeAccents($inflectionExplode[0])) {
 						if($wordDefExplode2[0] == $inflectionExplode[0]) {
 							echo "[DUPE] Definition for $inflectionExplode[0] will be copied to word $finalTestInflectionArray[$i]; Which means it'll look like |$finalTestInflectionArray[$i]	$wordDefExplode2[1]| COWABUNGA\n";
 							array_push($tempCatWordDefArray, $finalTestInflectionArray[$i]."	".$wordDefExplode2[1]);
@@ -381,4 +383,12 @@ Check these, check for `and` and `,` as a separator. Throw them out for now.
 	return $definitionToStrip;
 }
 //fram	alternative form of frem
+function removeAccents($str) {
+# The full list makes this script run 20 times longer, so we just use å æ and ø
+#		$a = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å',  'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å',  'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'Ā', 'ā', 'Ă', 'ă', 'Ą', 'ą', 'Ć', 'ć', 'Ĉ', 'ĉ', 'Ċ', 'ċ', 'Č', 'č', 'Ď', 'ď', 'Đ', 'đ', 'Ē', 'ē', 'Ĕ', 'ĕ', 'Ė', 'ė', 'Ę', 'ę', 'Ě', 'ě', 'Ĝ', 'ĝ', 'Ğ', 'ğ', 'Ġ', 'ġ', 'Ģ', 'ģ', 'Ĥ', 'ĥ', 'Ħ', 'ħ', 'Ĩ', 'ĩ', 'Ī', 'ī', 'Ĭ', 'ĭ', 'Į', 'į', 'İ', 'ı',  'Ĳ',  'ĳ', 'Ĵ', 'ĵ', 'Ķ', 'ķ', 'Ĺ', 'ĺ', 'Ļ', 'ļ', 'Ľ', 'ľ', 'Ŀ', 'ŀ', 'Ł', 'ł', 'Ń', 'ń', 'Ņ', 'ņ', 'Ň', 'ň', 'ŉ', 'Ō', 'ō', 'Ŏ', 'ŏ', 'Ő', 'ő',  'Œ',  'œ', 'Ŕ', 'ŕ', 'Ŗ', 'ŗ', 'Ř', 'ř', 'Ś', 'ś', 'Ŝ', 'ŝ', 'Ş', 'ş', 'Š', 'š', 'Ţ', 'ţ', 'Ť', 'ť', 'Ŧ', 'ŧ', 'Ũ', 'ũ', 'Ū', 'ū', 'Ŭ', 'ŭ', 'Ů', 'ů', 'Ű', 'ű', 'Ų', 'ų', 'Ŵ', 'ŵ', 'Ŷ', 'ŷ', 'Ÿ', 'Ź', 'ź', 'Ż', 'ż', 'Ž', 'ž', 'ſ', 'ƒ', 'Ơ', 'ơ', 'Ư', 'ư', 'Ǎ', 'ǎ', 'Ǐ', 'ǐ', 'Ǒ', 'ǒ', 'Ǔ', 'ǔ', 'Ǖ', 'ǖ', 'Ǘ', 'ǘ', 'Ǚ', 'ǚ', 'Ǜ', 'ǜ', 'Ǻ', 'ǻ',  'Ǽ',  'ǽ', 'Ǿ', 'ǿ', 'Ά', 'ά', 'Έ', 'έ', 'Ό', 'ό', 'Ώ', 'ώ', 'Ί', 'ί', 'ϊ', 'ΐ', 'Ύ', 'ύ', 'ϋ', 'ΰ', 'Ή', 'ή');
+#	$b = array('A', 'A', 'A', 'A', 'A', 'A', 'AE', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 's', 'a', 'a', 'a', 'a', 'a', 'a', 'ae', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'A', 'a', 'A', 'a', 'A', 'a', 'C', 'c', 'C', 'c', 'C', 'c', 'C', 'c', 'D', 'd', 'D', 'd', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'E', 'e', 'G', 'g', 'G', 'g', 'G', 'g', 'G', 'g', 'H', 'h', 'H', 'h', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'I', 'i', 'IJ', 'ij', 'J', 'j', 'K', 'k', 'L', 'l', 'L', 'l', 'L', 'l', 'L', 'l', 'l', 'l', 'N', 'n', 'N', 'n', 'N', 'n', 'n', 'O', 'o', 'O', 'o', 'O', 'o', 'OE', 'oe', 'R', 'r', 'R', 'r', 'R', 'r', 'S', 's', 'S', 's', 'S', 's', 'S', 's', 'T', 't', 'T', 't', 'T', 't', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'W', 'w', 'Y', 'y', 'Y', 'Z', 'z', 'Z', 'z', 'Z', 'z', 's', 'f', 'O', 'o', 'U', 'u', 'A', 'a', 'I', 'i', 'O', 'o', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'U', 'u', 'A', 'a', 'AE', 'ae', 'O', 'o', 'Α', 'α', 'Ε', 'ε', 'Ο', 'ο', 'Ω', 'ω', 'Ι', 'ι', 'ι', 'ι', 'Υ', 'υ', 'υ', 'υ', 'Η', 'η');
+	$a = array('å', 'Å', 'æ',  'Æ',  'ø', 'Ø');
+	$b = array('a', 'A', 'ae', 'AE', 'o', 'O');
+	return str_replace($a, $b, $str);
+}
 ?>
